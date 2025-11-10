aafc5e2d28c605aacf321343c7bb80229b3082cb
Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>
2025-11-10 15:59:40 +0100

    feat: add day 1 part 2 solution

--- src/main/java/com/jasinski/aoc2015/day1/Day1.java
+++ src/main/java/com/jasinski/aoc2015/day1/Day1.java
@@ -28,6 +28,8 @@ public class Day1 {
 
         // Santa starts at floor 0
         int floor = 0;
+        int basementEnteredAt = -1;
+        boolean basementWasEntered = false;
 
         // opening parenthesis means go up one floor
         // closing parenthesis means go down one floor
@@ -49,9 +51,16 @@ public class Day1 {
                     default:
                         break;
                 }
+
+                if (!basementWasEntered && floor == -1) {
+                    basementEnteredAt = i + 1; // chracters are counted from 1, not from 0
+                    basementWasEntered = true;
+                }
             }
         }
 
         System.out.printf("Instructions take Santa to the floor no " + floor + ".");
+        System.out.println();
+        System.out.printf("The index of a character that leads the Santa to the basement for the first time is " + basementEnteredAt + ".");
     }
 }
