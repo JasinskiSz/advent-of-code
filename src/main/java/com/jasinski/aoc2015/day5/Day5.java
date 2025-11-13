6a07d5de2f22c205a3b762449a5d0e7b95419e5f
Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>
2025-11-13 20:16:11 +0100

    feat: add day 5 part 2 solution

--- src/main/java/com/jasinski/aoc2015/day5/Day5.java
+++ src/main/java/com/jasinski/aoc2015/day5/Day5.java
@@ -5,9 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Day5 {
     public static void main(String[] args) {
@@ -56,6 +54,47 @@ public class Day5 {
                     return false;
                 })
                 .count());
+
+        // -----------------------
+        // Part 2 solution section
+        // -----------------------
+
+        // Part 2 introduced new rules and obsolete the old ones.
+        System.out.print("Number of Strings that are nice (part 2) is ");
+        System.out.println(lines.stream()
+                .filter(line -> {
+                    char[] letters = line.toCharArray();
+                    for (int i = 2; i < letters.length; i++) {
+                        if (letters[i] == letters[i - 2]) {
+                            return true;
+                        }
+                    }
+                    return false;
+                })
+                .filter(line -> {
+                    // take a single pair, two first letters, index 0 and 1
+                    // go over a whole string:
+                    // - start at index = 2
+                    // - compare a pair with two letters at index 2 and 3
+                    // - if a match, return true
+                    // if no match found, try taking pair from index 1 and 2 and do the above
+                    // if no more pairs, return false
+
+                    char[] letters = line.toCharArray();
+                    for (int i = 0; i < letters.length - 1; i++) {
+                        char[] pair = {letters[i], letters[i + 1]};
+
+                        // Go over a char array, starting from 2 and stopping at length - 1
+                        for (int j = i + 2; j < letters.length - 1; j++) {
+                            // here, compare two letters with a pair
+                            if (pair[0] == letters[j] && pair[1] == letters[j + 1]) {
+                                return true;
+                            }
+                        }
+                    }
+
+                    return false;
+                })
+                .count());
     }
 }
-
